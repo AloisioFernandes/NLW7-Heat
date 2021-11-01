8dcff4939c170de7487c95c1585c087535a1d468
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-11-01 20:56:42 -0300

    MessageList.

--- web_heat/src/components/MessageList/index.tsx
+++ web_heat/src/components/MessageList/index.tsx
@@ -1,5 +1,43 @@
+import styles from './styles.module.scss'
+
+import logoImg from '../../assets/logo.svg'
+
 export function MessageList() {
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar o evento, vamo lá 🔥🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/AloisioFernandes.png" alt="Aloisio Fernandes" />
+            </div>
+            <span>Aloísio Fernandes</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar o evento, vamo lá 🔥🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/AloisioFernandes.png" alt="Aloisio Fernandes" />
+            </div>
+            <span>Aloísio Fernandes</span>
+          </div>
+        </li>
+        
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar o evento, vamo lá 🔥🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/AloisioFernandes.png" alt="Aloisio Fernandes" />
+            </div>
+            <span>Aloísio Fernandes</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
