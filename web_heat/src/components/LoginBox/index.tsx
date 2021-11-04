dcbd7c6e8ebb997139efea818fa32fe59c42c627
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-11-03 22:50:17 -0300

    Redirecionando usuário logado do github.

--- web_heat/src/components/LoginBox/index.tsx
+++ web_heat/src/components/LoginBox/index.tsx
@@ -1,12 +1,26 @@
+import { useEffect } from 'react'
 import { VscGithubInverted } from 'react-icons/vsc'
 
 import styles from './styles.module.scss'
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=8c84e6fd48bb0edba2f9`
+
+  useEffect(() => {
+    const url = window.location.href
+    const hasGithubCode = url.includes('?code=')
+
+    if (hasGithubCode) {
+      const [ urlWithoutCode, githubCode ] = url.split('?code=')
+
+      window.history.pushState({}, '', urlWithoutCode)
+    }
+  }, [])
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Entrar com Github
       </a>
