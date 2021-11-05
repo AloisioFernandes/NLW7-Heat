8ab38d72c2848169a8f2d32d639e9184e5021775
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-11-04 22:44:46 -0300

    Redirecionando usuário logado do github completo.

--- web_heat/src/components/LoginBox/index.tsx
+++ web_heat/src/components/LoginBox/index.tsx
@@ -1,10 +1,32 @@
 import { useEffect } from 'react'
 import { VscGithubInverted } from 'react-icons/vsc'
 
+import { api } from '../../services/api'
+
 import styles from './styles.module.scss'
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
 
 export function LoginBox() {
   const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=8c84e6fd48bb0edba2f9`
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode
+    })
+
+    const { token, user } = response.data
+
+    localStorage.setItem('@dowhile:token', token)
+  }
 
   useEffect(() => {
     const url = window.location.href
@@ -14,6 +36,8 @@ export function LoginBox() {
       const [ urlWithoutCode, githubCode ] = url.split('?code=')
 
       window.history.pushState({}, '', urlWithoutCode)
+
+      signIn(githubCode)
     }
   }, [])
 
