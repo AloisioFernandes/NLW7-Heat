1dbddaa906ca5cc2fb694ef4e88195ced17878a6
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-11-08 21:11:34 -0300

    Mensagens cadastradas em tela e estilização.

--- web_heat/src/components/MessageList/index.tsx
+++ web_heat/src/components/MessageList/index.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import io from 'socket.io-client'
 
 import logoImg from '../../assets/logo.svg'
 
@@ -15,8 +16,30 @@ type Message = {
   }
 }
 
+const messagesQueue: Message[] = []
+
+const socket = io('http://localhost:4000')
+
+socket.on('new_message', newMessage => {
+  messagesQueue.push(newMessage)
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean)) // filtra valores undefined/null
+
+        messagesQueue.shift()
+      }
+    }, 3000)
+  }, [])
 
   useEffect(() => {
     api.get<Message[]>('messages/last3').then(response => {
