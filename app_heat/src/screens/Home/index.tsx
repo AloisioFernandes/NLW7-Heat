f55d98a74f04489afd0d479fc32e7d9b8b6454d1
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-11-12 23:04:24 -0300

    Estrutura inicial.

--- app_heat/src/screens/Home/index.tsx
+++ app_heat/src/screens/Home/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+import { styles } from './styles'
+
 export function Home() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>React Native</Text>
     </View>
   )
