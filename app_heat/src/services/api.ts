e37bef9aa837095cd03a7a929a493e39fb1a0225
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-12-03 22:38:58 -0300

    SignOut.

--- app_heat/src/services/api.ts
+++ app_heat/src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://10.0.0.108:4000'
+  // baseURL: 'http://10.0.0.108:4000'
+  baseURL: 'http://192.168.0.107:4000'
 })
