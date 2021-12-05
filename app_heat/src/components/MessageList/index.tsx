f644dacc69cfda3c144908265d26e0176206615e
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-12-04 21:10:12 -0300

    Buscando mensagens do backend.

--- app_heat/src/components/MessageList/index.tsx
+++ app_heat/src/components/MessageList/index.tsx
@@ -1,28 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { ScrollView } from 'react-native'
 
-import { Message } from '../Message'
+import { Message, MessageProps } from '../Message'
+
+import { api } from '../../services/api'
 
 import { styles } from './styles'
 
 export function MessageList() {
-  const message = {
-    id: '1',
-    text: 'mensagem de teste',
-    user: {
-      name: 'Al',
-      avatar_url: 'https://github.com/AloisioFernandes.png'
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([])
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const messagesResponse = await api.get<MessageProps[]>('/messages/last3')
+      setCurrentMessages(messagesResponse.data)
     }
-  }
+
+    fetchMessages()
+  }, [])
+
   return (
     <ScrollView 
       style={styles.container}
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps="never"
     >
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
+      {currentMessages.map(message => <Message key={message.id} data={message} />)}
     </ScrollView>
   )
 }
