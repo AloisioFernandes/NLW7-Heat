9e5a0c4522b1b77dc37d64cc255d059a037fe6eb
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-12-15 21:40:24 -0300

    Continuando controller.

--- tags_heat/lib/tags_heat/messages/create.ex
+++ tags_heat/lib/tags_heat/messages/create.ex
@@ -1,3 +1,13 @@
 defmodule TagsHeat.Message.Create do
   alias TagsHeat.{Message, Repo}
+
+  def call(params) do
+    params
+    |> Message.changeset()
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %Message{}} = result), do: result
+  defp handle_insert({:error, result}), do: {:error, %{result: result, status: :bad_request}}
 end
