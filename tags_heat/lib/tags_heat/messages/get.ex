bd9281d438a2f5d0ef29ce70db7a94cf96686ebc
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-12-20 21:09:24 -0300

    Continuando query get.

--- tags_heat/lib/tags_heat/messages/get.ex
+++ tags_heat/lib/tags_heat/messages/get.ex
@@ -1,7 +1,10 @@
 defmodule TagsHeat.Messages.Get do
+  import Ecto.Query
+
   alias TagsHeat.{Message, Repo}
 
   def today_messages do
-
+    today = Date.utc_today()
+    query = from message in Message, where: type(message.inserted_at, :date) == ^today
   end
 end
